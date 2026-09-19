af37dba096106202a8a6c998d1d11b38ff1ad551
Sergey Zinkin <zinkins@users.noreply.github.com>
2026-09-19 13:26:51 +0700

    Import family.home dashboard module

--- configuration.nix
+++ configuration.nix
@@ -13,6 +13,7 @@
       ./media.nix
       ./adguard.nix
       ./reverse-proxy.nix
+      ./homepage.nix
       ./ai-vpn.nix
       ./byparr.nix
       ./auto-update.nix
@@ -96,7 +97,7 @@
   #   wget
   # ];
 
-  # Some programs need SUID wrappers, can be configured further or are
+  # Some programs need SUID wrappers, can be configured further, or
   # started in user sessions.
   # programs.mtr.enable = true;
   # programs.gnupg.agent = {
@@ -121,22 +122,12 @@
   # system.copySystemConfiguration = true;
 
   # This option defines the first version of NixOS you have installed on this particular machine,
-  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
+  # and is used to maintain compatibility with application data (e.g. databases) created under older NixOS versions.
   #
   # Most users should NEVER change this value after the initial install, for any reason,
-  # even if you've upgraded your system to a new NixOS release.
+  # even if you've upgraded your NixOS release.
   #
-  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
-  # so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
-  # to actually do that.
-  #
-  # This value being lower than the current NixOS release does NOT mean your system is
-  # out of date, out of support, or vulnerable.
-  #
-  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
-  # and migrated your data accordingly.
-  #
-  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
-  system.stateVersion = "26.05"; # Did you read the comment?
-
+  # This value being lower than the current NixOS release does not mean
+  # the system is out of date. See `man configuration.nix` for details.
+  system.stateVersion = "26.05";
 }
