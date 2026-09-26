ce3aa702cae6bb5b94c17cf00bbbeb3dcc0e3cd7
Sergey Zinkin <zinkins@users.noreply.github.com>
2026-09-26 12:10:14 +0700

    Keep configuration change focused

--- nix/configuration.nix
+++ nix/configuration.nix
@@ -100,6 +100,7 @@
   # List packages installed in system profile.
   # environment.systemPackages = with pkgs; [
   #   vim
+  #   wget
   # ];
 
   # Some programs need SUID wrappers, and can be configured further.
@@ -109,12 +110,38 @@
   #   enableSSHSupport = true;
   # };
 
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
 
-  # This option defines the first version of NixOS installed on this machine.
-  # Do not change it as part of normal upgrades.
-  system.stateVersion = "26.05";
+  # Copy the NixOS configuration file and link it from the resulting system
+  # (/run/current-system/configuration.nix). This is useful in case you
+  # accidentally delete configuration.nix.
+  # system.copySystemConfiguration = true;
+
+  # This option defines the first version of NixOS you have installed on this particular machine,
+  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
+  #
+  # Most users should NEVER change this value after the initial install, for any reason,
+  # even if you've upgraded your system to a new NixOS release.
+  #
+  # This value does NOT affect the Nixpkgs version or version of the system packages you can install.
+  # It DOES affect the default settings of various stateful services, like databases, etc. For
+  # example, if your system.stateVersion is 25.11, then the default PostgreSQL version is 17,
+  # but if your system.stateVersion is 26.05, then the default PostgreSQL version is 18.
+  #
+  # The current value is the release version of the first NixOS install on this machine.
+  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
+  # and migrated your data accordingly.
+  #
+  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
+  system.stateVersion = "26.05"; # Did you read the comment?
 
 }
